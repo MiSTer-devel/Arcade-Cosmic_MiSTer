33b7ede2a4660c1e1f03d48a4cfd9e9715f064ae
Michael Coates <56591865+MacroFPGA@users.noreply.github.com>
2024-12-17 21:01:41 +0000

    Update SYS

--- Arcade-Cosmic.sv
+++ Arcade-Cosmic.sv
@@ -58,6 +58,7 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM (USE_FB=1 in qsf)
@@ -189,6 +190,7 @@ assign VGA_F1 = 0;
 assign VGA_SCALER = 0;
 assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
 assign FB_FORCE_BLANK = 0;
 
 assign LED_USER  = 0;
